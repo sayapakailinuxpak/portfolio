6cedd717da89c9e2a4108bcb9325d70a7bfe2aa2
sayapakailinuxpak <sayapakailinuxpak@gmail.com>
2023-05-16 01:37:06 +0700

    adding main files

---
+++ script.js
@@ -0,0 +1,17 @@
+// const days = document.getElementById("days");
+// const hours = document.getElementById("hours");
+// const minutes = document.getElementById("minutes");
+// const seconds = document.getElementById("seconds");
+
+
+function countDownTimer() {
+    const currentDate = Date.now();
+    const deadlineDate = Date.UTC(2023, 6 + 1, 31);
+    
+    setInterval(() => {
+        
+    }, 1000)
+    
+}
+
+countDownTimer()
